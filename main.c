347a9d8898f1883596b0c28ade9fe75f07dacf29
Nelson Rueda <nelsonrueda@wposs.com>
2022-07-13 16:00:24 -0500

    IMPLEMENTACIÓN - Se implmento la verificación de la pelota
    - Se implementa metodo que lee hubicación de la pelota en la matriz.
    - Se implemeta método que valida si la pelota se esta hubicada en alguno de los bordes o en las raquetas.

--- main.c
+++ main.c
@@ -2,24 +2,35 @@
 #include"metodos.h"
 
 
+
+
+
 int main()
 {
 
+    //VARIABLES DE POSICIÓN///////////////////////
+    int pelX; //posición de la pelota en el eje X
+    int pelY; //posición de la pelota en el eje Y
 
-    int pelX;
-    int pelY;
-    int iniRaquet;
-    int finRaquet;
-    int iniRaquetIA;
-    int finRaquetIA;
+    int iniRaquet;  //posición inicial de la raqueta del jugador
+    int finRaquet;  //posición final de la raqueta del jugador
 
-    char campo [V][H];
+    int iniRaquetIA;  //posición inicial de la raqueta de la IA
+    int finRaquetIA;  //posición final de la raqueta de la IA
+    ////////////////////////////////////////////////
 
+
+
+    char campo [V][H];  //matriz de char la cual hace de campo en el que se ejecutara el juego
+
+
+    //VARIABLES DE MODIFICACIÓN
     int modX;
     int modY;
     int modIA;
+    /////////////////////////////
 
-
+    //possición //////////////////
     pelX = 37;
     pelY = 10;
 
@@ -28,9 +39,14 @@ int main()
 
     iniRaquetIA = 3;
     finRaquetIA = 18;
+    /////////////////////////////
 
+    //modificación//////////
+    modX = modY = modIA = -1;
+    ///////////////////////
 
     inicio(campo, pelX,pelY, iniRaquet, finRaquet, iniRaquetIA, finRaquetIA);
+    gameloop(campo, pelX,pelY, iniRaquet, finRaquet, iniRaquetIA, finRaquetIA, modX, modY, modIA);
 
     system("pause");
     return 0;
--- metodos.h
+++ metodos.h
@@ -1,5 +1,12 @@
 #define V 21
 #define H 75
+
+#define RAQUETA_JUGADOR 177 //caracter en ASCII que se utiliza para graficar la raqueta del jugador
+#define RAQUETA_IA 178  //caracter en ASCII que se utiliza para graficar la raqueta de la IA
+#define BORDE_VERTICAL 219  //caracter en ASCII que se utiliza para graficar el borde vertical del campo de juego
+#define BORDE_HORIZONTAL  254  //caracter en ASCII que se utiliza para graficar el borde horizontal del campo de juego
+
+
 void borde (char campo[V][H]);
 void raquetaJugador(char campo [V][H], int iniRaquet, int finRaqueta);
 void raquetaIA(char campo [V][H], int iniRaquetIA, int finRaquetIA);
